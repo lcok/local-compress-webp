7fca9ca32e6da292bda136f777a321f9ac6af2fc
lcok <24971969+lcok@users.noreply.github.com>
2024-11-16 17:50:26 +0800

    compress to webp locally

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## picgo-plugin-local-compress-webp
 
 Convert image compression to webp format locally
+
+
+## TODO
+
+- 可配置：压缩参数
+- 可配置：是否保留转换后的图片文件
+- 可配置：转换后图片的存放目录
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import path from 'path';
+import sharp from 'sharp';
+import * as fs from 'fs';
+import { PicGo } from 'picgo'
+
+// 插件名称，方便日志标识
+const PLUGIN_NAME = 'local-compress-webp';
+
+export = (ctx: PicGo) => {
+  const handle = async (ctx: PicGo): Promise<PicGo> => {
+    let [iptImgPath] = ctx.input;
+    let iptImgExt = path.extname(iptImgPath);
+
+    // 如果是webp格式，不需要转换，直接返回
+    if (iptImgExt === '.webp') {
+      ctx.log.info(`[${PLUGIN_NAME}] 已是 webp 格式，跳过转换.`);
+      return ctx;
+    }
+
+    if (!fs.existsSync(iptImgPath)) {
+      // 文件不存在时的日志
+      const errMsg = `[${PLUGIN_NAME}] 转换失败: 未找到原文件路径: ${iptImgPath}`;
+      ctx.log.error(errMsg);
+      return ctx;
+    }
+
+    // 转换后的图片路径
+    const optImgPath = path.join(path.dirname(iptImgPath), path.basename(iptImgPath, iptImgExt) + '.webp');
+
+    try {
+      await sharp(iptImgPath)
+        .webp({ quality: 75 })
+        .toFile(optImgPath);
+
+      // 转换完成后的日志
+      ctx.input = [optImgPath];
+      ctx.log.info(`[${PLUGIN_NAME}] 转换成功: ${optImgPath}`);
+
+    } catch (error) {
+      // 错误日志
+      const errorMsg = `[${PLUGIN_NAME}] 转换失败: ${error.message}`;
+      ctx.log.error(errorMsg);
+    }
+
+    return ctx;
+  }
+
+  const register = (): void => {
+    ctx.helper.beforeTransformPlugins.register(PLUGIN_NAME, {
+      handle
+    });
+  }
+
+  return {
+    register
+  }
+}
